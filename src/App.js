cabf5d8be2c64440192280eba6d88a423fc180f0
SuperBatata <khaled.battiche@esprit.tn>
2022-11-24 19:34:28 +0100

    added home file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './home';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         hello.
         </p>
+
+
+        <Home />
         <a
           className="App-link"
           href="https://reactjs.org"
